5e7e44850104666f54c3eed0e75a8e73d1ccac29
Rob <robmoore121@gmail.com>
2017-10-05 15:16:22 +0100

    hole 4

--- src/main/java/com/develogical/golf/hole4/Hole4After.java
+++ src/main/java/com/develogical/golf/hole4/Hole4After.java
@@ -1,17 +1,30 @@
 package com.develogical.golf.hole4;
 
 public class Hole4After {
+    public void printReceipt() {
+        printName("Mr J Smith");
+        printAddress("34", "High Street", "Oxford", "OX1 1TT");
+        printItem("1 Book", "5.99");
+        printItem("1 CD", "9.99");
+        printTotal("15.98");
+    }
 
-    public void printLabelAndReceipt() {
+    private void printName(String name) {
+        System.out.println(name);
+    }
 
-        System.out.println("Mr J Smith");
-        System.out.println("34 High Street");
-        System.out.println("Oxford");
-        System.out.println("OX1 1TT");
-        System.out.println("1 Book, 5.99");
-        System.out.println("1 CD, 9.99");
-        System.out.println("Total: 15.98 ");
+    private void printAddress(String houseNameOrNumber, String streetName, String townOrCity, String postcode) {
+        System.out.println(houseNameOrNumber + " " + streetName);
+        System.out.println(townOrCity);
+        System.out.println(postcode);
+    }
 
+    private void printItem(String item, String price) {
+        System.out.println(item + ", " + price);
+    }
+
+    private void printTotal(String amount) {
+        System.out.println("Total: " + amount);
     }
 
 }
